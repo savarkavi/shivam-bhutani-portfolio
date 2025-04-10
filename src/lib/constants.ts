e954d5663f62fada34bc6ecdf1df99bf48838c04
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-10 09:03:02 +0530

    Add navigation and social items constants to constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+export const navigationItems = [
+  { id: 1, name: "Home", href: "/" },
+  { id: 2, name: "Work", href: "/work" },
+  { id: 3, name: "About", href: "/about" },
+  { id: 4, name: "Contact", href: "/contact" },
+];
+
+export const soacialItems = [
+  { id: 1, name: "Instagram", href: "/instagram" },
+  { id: 2, name: "X (Twitter)", href: "/x" },
+  { id: 3, name: "Youtube", href: "/youtube" },
+];
