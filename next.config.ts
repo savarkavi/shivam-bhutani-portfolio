0825f953d2e849747ac8b8a3b857c47c5c0ac633
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-10 09:07:47 +0530

    Add remote image pattern for external hostname in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "5ct1dh56fd.ufs.sh",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
