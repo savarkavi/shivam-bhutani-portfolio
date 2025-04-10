90980c35902b3ecc21fc770a530e5175239f02fb
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-10 09:07:38 +0530

    Refactor layout to use Cormorant Garamond font, add Footer component, and adjust structure for Header and children elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Cormorant_Garamond } from "next/font/google";
 import Header from "@/components/Header";
 import { ReactLenis } from "lenis/react";
+import Footer from "@/components/Footer";
+import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const cormorantGaramond = Cormorant_Garamond({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -31,11 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <ReactLenis root>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <Header />
-          {children}
+        <body className={`${cormorantGaramond.className} antialiased`}>
+          <Footer />
+          <div className="bg-white z-[99] mb-[100vh]">
+            <Header />
+            {children}
+          </div>
         </body>
       </ReactLenis>
     </html>
