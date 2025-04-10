fa4207bbdb3a8b19254af65310abdf1e3bda71a0
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-10 09:06:36 +0530

    Refactor Header component with deifferent layout and content and add navigation items

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,25 @@
-import { lavishlyYours } from "@/fonts/fonts";
+import { navigationItems } from "@/lib/constants";
+import Link from "next/link";
+import LinkWrapper from "./LinkWrapper";
+import { boska } from "@/fonts/fonts";
 
 const Header = () => {
   return (
-    <div className="w-full h-14 p-8 flex justify-between items-center z-[99] sticky top-0 bg-[#ecebeb]">
-      <div className="flex flex-col font-serif text-lg">
-        <h2 className="text-gray-600 font-semibold">Location:</h2>
-        <p className="font-semibold">India, Delhi (10:00pm)</p>
+    <div className="header w-full h-fit px-4 py-4 lg:px-8 flex justify-between items-center sticky top-0">
+      <div className="hidden xl:flex flex-col text-xl font-semibold">
+        <span className={`${boska.className} text-9xl font-bold uppercase`}>
+          Shivam Bhutani
+        </span>
       </div>
-      <h1
-        className={`${lavishlyYours.className} text-4xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2`}
-      >
-        Shivam Bhutani
-      </h1>
-      <h2 className="font-serif text-lg">Menu</h2>
+      <div className="text-xl font-semibold uppercase">
+        <div className="flex items-center gap-6 font-bold">
+          {navigationItems.map((item) => (
+            <LinkWrapper key={item.id} color="bg-black">
+              <Link href={item.href}>{item.name}</Link>
+            </LinkWrapper>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
