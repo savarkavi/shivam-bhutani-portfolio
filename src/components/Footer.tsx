6666611e9f4e6747ae564a0e5224d20bbdcc4875
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-12 16:34:58 +0530

    Refactor Footer component for improved layout and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,38 +1,31 @@
 "use client";
 
-import { lavishlyYours, stardom } from "@/fonts/fonts";
 import { navigationItems, soacialItems } from "@/lib/constants";
 import Link from "next/link";
 import LinkWrapper from "./LinkWrapper";
 
 const Footer = () => {
   return (
-    <div className="h-screen fixed w-full bottom-0 left-0 bg-stone-950 flex flex-col z-[-10] gap-20 items-center justify-between text-white py-28">
-      <div className="text-center">
-        <h2 className={`${lavishlyYours.className} text-8xl`}>
-          Shivam Bhutani
-        </h2>
-        <h2 className={`${stardom.className} text-9xl`}>Photography</h2>
-      </div>
-      <div className="flex flex-col items-center gap-6">
-        <div className="flex items-center gap-10 text-7xl font-bold">
-          {navigationItems.map((item) => (
-            <Link
-              href={item.href}
-              key={item.id}
-              className="hover:text-gray-500 transition-all"
-            >
-              {item.name}
-            </Link>
-          ))}
-        </div>
-        <div className="flex items-center gap-4 text-3xl">
-          <span className="text-gray-500">Follow on</span>
-          {soacialItems.map((item) => (
-            <LinkWrapper key={item.id} color="bg-white">
-              <Link href={item.href}>{item.name}</Link>
-            </LinkWrapper>
-          ))}
+    <div className="fixed bottom-0 left-0 z-[-10] flex h-[50%] w-full flex-col justify-between bg-stone-950 px-4 py-16 text-white xl:px-16 xl:py-20">
+      <div className="flex flex-col justify-between gap-10 lg:flex-row">
+        <h2 className={`text-2xl uppercase`}>Shivam Bhutani</h2>
+        <div className="flex justify-between text-lg lg:gap-40">
+          <div className="flex flex-col gap-2">
+            <span className="text-gray-400">Navigation</span>
+            {navigationItems.map((item) => (
+              <LinkWrapper key={item.id} color="bg-white">
+                <Link href={item.href}>{item.name}</Link>
+              </LinkWrapper>
+            ))}
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-gray-400">Follow on</span>
+            {soacialItems.map((item) => (
+              <LinkWrapper key={item.id} color="bg-white">
+                <Link href={item.href}>{item.name}</Link>
+              </LinkWrapper>
+            ))}
+          </div>
         </div>
       </div>
     </div>
