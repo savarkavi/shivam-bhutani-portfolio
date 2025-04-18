8fa12a3c08bdcd5d3f40b6b820b0f824095f28c4
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-18 11:40:41 +0530

    Remove unused components, styles, and configurations to streamline the project. This includes deleting various pages, components, fonts, and configuration files

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import AboutContentWrapper from "@/components/about-page/AboutContentWrapper";
-
-export default function Page() {
-  return (
-    <div>
-      <AboutContentWrapper />
-    </div>
-  );
-}
--- src/app/works/page.tsx
+++
@@ -1,9 +0,0 @@
-import WorksContentWrapper from "@/components/works-page/WorksContentWrapper";
-
-export default function Page() {
-  return (
-    <div className="min-h-screen">
-      <WorksContentWrapper />
-    </div>
-  );
-}
