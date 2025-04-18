996a44581b3778e95d58919f11228ba3783f3325
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-18 10:22:51 +0530

    Add About page components including AboutContentWrapper, Bio, MyPhilosophy, and Instagram

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutContentWrapper from "@/components/about-page/AboutContentWrapper";
+
+export default function Page() {
+  return (
+    <div>
+      <AboutContentWrapper />
+    </div>
+  );
+}
