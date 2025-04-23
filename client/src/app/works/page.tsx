47161d8e9079c70c2c4bf1b493938fba88e5c76e
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-23 10:22:54 +0530

    feat: integrate data fetching for works page and enhance WorksContentWrapper component

--- client/src/app/works/page.tsx
+++ client/src/app/works/page.tsx
@@ -1,9 +1,15 @@
 import WorksContentWrapper from "@/components/works-page/WorksContentWrapper";
+import { client } from "@/sanity/client";
+import { WorksPage } from "@/sanity/types";
 
-export default function Page() {
+const WORKS_QUERY = `*[_type == "worksPage"][0]`;
+
+export default async function Page() {
+  const data: WorksPage = await client.fetch(WORKS_QUERY);
+
   return (
     <div className="min-h-screen">
-      <WorksContentWrapper />
+      <WorksContentWrapper data={data} />
     </div>
   );
 }
--- client/src/components/works-page/WorksContentWrapper.tsx
+++ client/src/components/works-page/WorksContentWrapper.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { worksCategories } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
 import LinkWrapper from "../LinkWrapper";
@@ -8,62 +7,75 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/all";
 import { useRef } from "react";
+import { WorksPage } from "@/sanity/types";
+import { urlFor } from "../../lib/utils";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
-const WorksContentWrapper = () => {
+const WorksContentWrapper = ({ data }: { data: WorksPage }) => {
   const container = useRef(null);
 
   return (
-    <div ref={container} className="h-full w-full px-10 py-16">
-      <div className="section-title z-[10] mt-6 w-full border-t pt-10">
-        <h1 className={`text-9xl uppercase`}>Works</h1>
+    <div ref={container} className="h-full w-full py-16 xl:px-10">
+      <div className="section-title z-[10] mt-6 w-full px-8 text-center xl:px-0 xl:text-start">
+        <div className="w-full border-t pt-10">
+          <h1 className="text-8xl uppercase xl:text-9xl">{data.pageTitle}</h1>
+        </div>
       </div>
       <div className="mx-auto mt-20 flex h-full w-full flex-col items-center justify-between gap-16">
-        <div className="flex flex-[30%] gap-12">
+        <div className="flex flex-[30%] flex-col items-center gap-6 px-8 xl:flex-row xl:gap-12 xl:px-0">
           <span className="text-xl text-gray-600 uppercase">Index</span>
-          <div className="flex gap-12">
-            {worksCategories.map((item) => (
+          <div className="flex flex-wrap justify-center gap-8 xl:gap-12">
+            {data.workCategories?.map((item, i) => (
               <Link
-                key={item.id}
-                href={item.href}
+                key={item._key}
+                href={`${item.slug?.current}`}
                 className="w-fit text-xl font-semibold"
               >
-                {`[${item.id}] ${item.link}`}
+                {`[${i}] ${item.sectionTitle}`}
               </Link>
             ))}
           </div>
         </div>
-        <div className="flex h-full w-full flex-col gap-20">
-          {worksCategories.map((item, index) => (
-            <div
-              key={item.id}
-              className={`flex items-center gap-20 ${
-                index % 2 === 1 ? "flex-row-reverse" : ""
-              }`}
-            >
-              <div className="group relative h-[1200px] w-full cursor-pointer overflow-hidden transition-all hover:grayscale-75">
-                <Image
-                  src={item.img}
-                  alt={item.name}
-                  fill
-                  className="scale-105 object-cover transition-all duration-500 group-hover:scale-100"
-                />
-              </div>
-              <div className="flex flex-col gap-10">
-                <div className="flex items-center justify-between">
-                  <h2 className="text-6xl uppercase">{item.name}</h2>
-                  <p>{`${item.totalPhotos} photos`}</p>
+        <div className="mt-20 flex h-full w-full flex-col gap-20 xl:mt-0">
+          {data.workCategories?.map((item, index) => {
+            const coverImageUrl = item.coverImage
+              ? urlFor(item.coverImage)?.url()
+              : null;
+            return (
+              <div
+                key={item._key}
+                className={`flex flex-col items-center gap-20 xl:flex-row ${
+                  index % 2 === 1 ? "xl:flex-row-reverse" : ""
+                }`}
+              >
+                <div className="group relative h-[1200px] w-full cursor-pointer overflow-hidden transition-all hover:grayscale-75">
+                  <Image
+                    src={coverImageUrl || "https://placeholder.co/550x310/png"}
+                    alt={item.coverImage?.alt || "cover image"}
+                    fill
+                    className="scale-105 object-cover transition-all duration-500 group-hover:scale-100"
+                  />
                 </div>
-                <p className="max-w-[1000px] text-lg">{item.desc}</p>
-                <LinkWrapper color="bg-black">
-                  <Link href={item.href} className="text-lg">
-                    See album
-                  </Link>
-                </LinkWrapper>
+                <div className="flex flex-col gap-10 px-4 xl:px-0">
+                  <div className="flex items-center justify-between">
+                    <h2 className="text-4xl uppercase lg:text-6xl">
+                      {item.sectionTitle}
+                    </h2>
+                    <p>{`21 photos`}</p>
+                  </div>
+                  <p className="max-w-[800px] text-sm lg:text-lg xl:max-w-[1000px]">
+                    {item.description}
+                  </p>
+                  <LinkWrapper color="bg-black">
+                    <Link href={`${item.slug?.current}`} className="text-lg">
+                      See album
+                    </Link>
+                  </LinkWrapper>
+                </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </div>
