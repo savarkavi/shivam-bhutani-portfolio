7a9003a8827780927dd1462fcb24cb0ebbe364f6
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-29 19:22:08 +0530

    feat: refactor AlbumContainer to improve type safety and enhance image handling

--- client/src/components/gallery-pages/AlbumContainer.tsx
+++ client/src/components/gallery-pages/AlbumContainer.tsx
@@ -4,28 +4,21 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import Image from "next/image";
-import { GalleryPage } from "@/sanity/types";
 import { urlFor } from "@/lib/utils";
 import { useState } from "react";
 import { LuGalleryHorizontal } from "react-icons/lu";
 import { RiGalleryView2 } from "react-icons/ri";
+import { GalleryPageData } from "@/sanity/customTypes";
 
-const AlbumContainer = ({
-  album,
-  data,
-}: {
-  album: string;
-  data: GalleryPage;
-}) => {
+const AlbumContainer = ({ data }: { album: string; data: GalleryPageData }) => {
   const [isAlbumMode, setIsAlbumMode] = useState(false);
 
-  const albumData = data.albums?.filter(
-    (item) => item.slug?.current === album,
-  )[0];
-
-  const albumImages = albumData?.images?.map((item) => item);
+  const albumImages = data?.album?.images?.map((item) => item);
   const albumImagesUrl = albumImages?.map((item) =>
     item ? urlFor(item)?.format("webp").quality(20).url() : null,
+  );
+  const albumImagesLQIP = data.album?.images?.map(
+    (image) => image.asset?.metadata?.lqip,
   );
 
   const settings = {
@@ -37,37 +30,53 @@ const AlbumContainer = ({
     speed: 2000,
     autoplaySpeed: 100,
     easing: "linear",
-    variableWidth: true,
     centerMode: true,
     focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <div className="relative flex min-h-screen flex-col items-center justify-between gap-10 pt-10 pb-6">
       {!isAlbumMode ? (
-        <Slider {...settings} className="w-full overflow-hidden">
+        <Slider
+          {...settings}
+          className="h-[500px] w-full overflow-hidden lg:h-full"
+        >
           {albumImages?.map((item, i) => (
-            <div key={item._key} className="h-[700px] p-1 hover:cursor-grab">
+            <div
+              key={item._key}
+              className="relative h-[500px] w-[500px] hover:cursor-grab lg:h-[700px]"
+            >
               <Image
                 src={albumImagesUrl?.[i] || ""}
                 alt="portrait image"
-                width={1200}
-                height={700}
-                className="h-[700px] w-auto"
+                fill
+                className="object-contain"
+                placeholder={albumImagesLQIP?.[i] ? "blur" : "empty"}
+                blurDataURL={albumImagesLQIP?.[i]}
               />
             </div>
           ))}
         </Slider>
       ) : (
-        <div className="mt-10 flex w-full flex-wrap gap-16 px-10">
+        <div className="mt-10 flex w-full flex-wrap justify-center gap-16 px-10">
           {albumImages?.map((item, i) => (
-            <div key={item._key} className="h-500px] shadow-lg">
+            <div key={item._key} className="relative h-[500px] w-[500px]">
               <Image
                 src={albumImagesUrl?.[i] || ""}
                 alt="portrait image"
-                width={1200}
-                height={700}
-                className="h-[500px] w-auto"
+                fill
+                className="h-[500px] w-auto object-contain"
+                placeholder={albumImagesLQIP?.[i] ? "blur" : "empty"}
+                blurDataURL={albumImagesLQIP?.[i]}
               />
             </div>
           ))}
