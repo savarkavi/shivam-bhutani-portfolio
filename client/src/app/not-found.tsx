93de154eabae26070556f41912830bc2ca3f3a2f
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-30 11:51:28 +0530

    fix: update NotFound component layout and content for improved user experience

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center gap-10 text-4xl">
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex h-full flex-col items-center justify-center gap-6 py-12 text-2xl">
+      <div className="relative h-[300px] w-[250px]">
+        <Image
+          src="/shivam-portrait.webp"
+          alt="shivam portrait image"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <p>The page you are looking for was not found :(</p>
+      <Link href="/" className="text-blue-600">
+        Return Home
+      </Link>
     </div>
   );
 }
