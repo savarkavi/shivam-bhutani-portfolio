1c7b47a3b813cd0ed64f5c49c45494c31fecdbd3
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-05-06 10:59:47 +0530

    refactor: remove unused socialItems constant from constants.ts

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -4,9 +4,3 @@ export const navigationItems = [
   { id: 3, name: "About", href: "/about" },
   { id: 4, name: "Contact", href: "mailto:shivambhutaniphotography@gmail.com" },
 ];
-
-export const soacialItems = [
-  { id: 1, name: "Instagram", href: "/instagram" },
-  { id: 2, name: "X (Twitter)", href: "/x" },
-  { id: 3, name: "Youtube", href: "/youtube" },
-];
