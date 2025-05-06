5cf869d97a6b9c120197989515bd56582496722c
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-05-06 11:00:06 +0530

    refactor: remove unused soacialItems and update footer to fetch data dynamically

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,14 @@
-"use client";
-
-import { navigationItems, soacialItems } from "@/lib/constants";
+import { navigationItems } from "@/lib/constants";
 import Link from "next/link";
 import LinkWrapper from "./LinkWrapper";
+import { client } from "@/sanity/client";
+import type { Footer } from "@/sanity/types";
 
-const Footer = () => {
+const FOOTER_QUERY = `*[_type == "footer"][0]`;
+
+const Footer = async () => {
+  const data: Footer = await client.fetch(FOOTER_QUERY);
+
   return (
     <div className="bottom-0 left-0 flex h-[450px] w-full flex-col justify-between bg-stone-950 px-4 pt-16 pb-10 text-white xl:px-16 xl:pt-18">
       <div className="flex flex-col justify-between gap-10 lg:flex-row">
@@ -20,16 +24,18 @@ const Footer = () => {
           </div>
           <div className="flex flex-col gap-2">
             <span className="text-gray-400">Follow on</span>
-            {soacialItems.map((item) => (
-              <LinkWrapper key={item.id} color="bg-white">
-                <Link href={item.href}>{item.name}</Link>
+            {data.footerLinks?.map((item) => (
+              <LinkWrapper key={item._key} color="bg-white">
+                <Link href={item.footerLinkUrl || "#"}>
+                  {item.footerLinkName}
+                </Link>
               </LinkWrapper>
             ))}
           </div>
         </div>
       </div>
       <div className="flex w-full justify-between gap-10 text-sm sm:text-base">
-        <p>Copyright Shivam Bhutani ©2025</p>
+        <p>{data.copyrightText}</p>
         <p>
           Website by{" "}
           <Link href="mailto:sushant20.sharma00@gmail.com">Sushant</Link>
